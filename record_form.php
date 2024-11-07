b4851b0442bb00120066249fe0b7f31a34a7a8ec
Кирилл Молчанов <email@example.com>
2024-11-07 20:24:49 +0300

    внес изменения в 7 строку

--- record_form.php
+++ record_form.php
@@ -4,7 +4,7 @@
 }
 //Пишу сюда изменение
 use Bitrix\Main\Localization\Loc;
-
+//Пишу сюда ещё одно изменение
 $startTimeInputName = $arResult['WORKTIME_RECORD_FORM_NAME'] . '[recordedStartTime]';
 $endTimeInputName = $arResult['WORKTIME_RECORD_FORM_NAME'] . '[recordedStopTime]';
 $breakLengthInputName = $arResult['WORKTIME_RECORD_FORM_NAME'] . '[recordedBreakLengthTime]';
